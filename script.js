dfc4bc1ecdab33702586b36684c4d0461da585ae
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-09-14 20:14:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count =1;
+	for(let i=0;i<str.lenght;i++){
+		if(str[i]== str[i+1]){
+			count++;
+		}else {
+			return null;
+		}
+		
+	}
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+firstNonRepeatedChar();
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
